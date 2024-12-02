c66d38ed8ebb1ac9a2cfe57ad01cfa072074c80c
Johanng250 <johanng250@gmail.com>
2024-12-02 13:59:35 +0000

    data

---
+++ DATA_LAYER.py
@@ -0,0 +1,22 @@
+class EmployeeData:
+    pass
+
+class ProbertyData:
+    pass
+
+
+class LocationData:
+    pass
+
+class MaintenanceData:
+    pass
+
+class MaintenanceReportData:
+    pass
+
+class ContractorReport:
+    pass
+
+
+
+
