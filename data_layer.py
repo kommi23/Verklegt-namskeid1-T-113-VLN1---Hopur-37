a6a142adb12bf2a8f3e6766c9956418f107b9638
kommi23 <kormakurbjarna@gmail.com>
2024-12-03 12:16:46 +0000

    bætti við data og csv

--- data_layer.py
+++ data_layer.py
@@ -1,50 +1,83 @@
 import csv
 
 class EmployeeData:
-    def init(self):
+    def __init__(self):
         pass
     def add_employee(self, employee):
         with open("employees.csv", "a") as csv_file:
             csv_file.write(employee)
     def get_employees(self):
+        employees = []
         with open("employees.csv", "r") as csv_file:
             csv_reader = csv.reader(csv_file)
-            return csv_reader
+            for line in csv_reader:
+                employees.append(line)
+            return employees
         
 class ProbertyData:
-    def init(self):
-        self.proberties = []
+    def __init__(self):
+        pass
     def add_property(self, proberty):
-        self.proberties.append(proberty)
+        with open("properties.csv", "a") as csv_file:
+            csv_file.write(proberty)
     def get_proberty(self):
-        return self.proberties
+        properties = []
+        with open("properties.csv") as csv_file:
+            csv_reader = csv.reader(csv_file)
+            for line in csv_reader:
+                properties.append(line)
+        return properties
 
 class LocationData:
-    def init(self):
-        self.locations = []
+    def __init__(self):
+        pass
     def get_locations(self):
-        return self.locations
+        locations = []
+        with open("locations.csv", "r") as csv_file:
+            csv_reader = csv.reader(csv_file)
+            for line in csv_reader:
+                locations.append(line)
+        return locations
 
 class MaintenanceData:
-    def init(self):
-        self.maintenances = []
+    def __init__(self):
+        pass
     def add_maintenance(self, maintenance):
-        self.maintenances.append(maintenance)
+        with open("maintenances.csv", "a") as csv_file:
+            csv_file.write(maintenance)
     def get_maintenences(self):
-        return self.maintenances
+        maintenances = []
+        with open("maintenances.csv", "r") as csv_file:
+            csv_reader = csv.reader(csv_file)
+            for line in csv_reader:
+                maintenances.append(line)
+        return maintenances
 
 class MaintenanceReportData:
-    def init(self):
-        self.maintenancereport = []
+    def __init__(self):
+        pass
     def add_maintenancereport(self, maintenancereport):
-        self.maintenancereport.append(maintenancereport)
+        with open("maintenancereports.csv", "a") as csv_file:
+            csv_file.write(maintenancereport)        
     def get_maintenanreport(self):
-        return self.maintenancereport
+        with open("maintenancereports.csv", "r") as csv_file:
+            maintenancereports = []
+            csv_reader = csv.reader(csv_file)
+            for line in csv_reader:
+                maintenancereports.append(line)
+        return maintenancereports
 
 class ContractorReport:
-    def init(self):
-        self.ContractorReports = []
+    def __init__(self):
+        pass
     def add_contractoreports(self, contractorreport):
-        self.ContractorReports.append(contractorreport)
+        with open("contractorreports.csv", "a") as csv_file:
+            csv_file.write(contractorreport)
     def get_contractorReports(self):
-        return self.add_contractoreports
+        contractorreports = []
+        with open("contractorreports.csv", "r") as csv_file:
+            contractorreports = []
+            csv_reader = csv.reader(csv_file)
+            for line in csv_reader:
+                contractorreports.append(line)
+        return contractorreports      
