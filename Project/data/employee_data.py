a5588c96edaec6b04856f3bf1112130263517c4b
victor23-dev <victor23@ru.is>
2024-12-04 20:25:23 +0000

    data wrapper test

--- Project/data/employee_data.py
+++ Project/data/employee_data.py
@@ -13,15 +13,15 @@ class EmployeeData:
     def get_employees(self):
         employees = []
         try:
-            with open("employees.csv", "r", newline='', encoding='utf-8') as csv_file:
+            with open("Project/data/csv_files/employees.csv", "r", newline='', encoding='utf-8') as csv_file:
                 csv_reader = csv.reader(csv_file)
                 for line in csv_reader:
                     employees.append(line)
             return employees
         except: raise 
-    def get_singular_employee(self, employee, newline='', encoding='utf-8'): 
+    def get_singular_employee(self): 
         try:    
-                with open("employees.csv", "r") as csv_file:
+                with open("Project/data/csv_files/employees.csv", "r") as csv_file:
                     for line in csv_file:
                         line = line.split(",")
                         if self in line[0]:
