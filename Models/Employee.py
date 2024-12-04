8174f60f4587f75f6d4c02bcdf539123478ac397
Viktor Már Heiðarsson <viktormar123@gmail.com>
2024-12-04 14:07:09 +0000

    Model klasar fyrir manager emplloyee og cotractor

--- Models/Employee.py
+++ Models/Employee.py
@@ -1,16 +1,32 @@
 class Employee:
-    """docstring for Employee"""
-    def __init__(self, employee_id: str, name: str, location: str, phone: str, email: str, address: str):
+    def __init__(self, employee_id, name, social_security_number, address, landline, mobile_phone, email, location):
         self.employee_id = employee_id
         self.name = name
+        self.social_security_number = social_security_number
+        self.address = address
+        self.landline = landline
+        self.mobile_phone = mobile_phone
+        self.email = email
+        self.location = location
+
+
+class Manager:
+    def __init__(self, manager_id, name, social_security_number, address, landline, mobile_phone, email, location):
+        self.manager_id = manager_id
+        self.name = name
+        self.social_security_number = social_security_number
+        self.address = address
+        self.landline = landline
+        self.mobile_phone = mobile_phone
+        self.email = email
+        self.location = location
+
+class Contractor:
+    def __init__(self, name, contact_name, location, phone, address, opening_hours):
+        self.name = name
+        self.contact_name = contact_name
         self.location = location
         self.phone = phone
-        self.email = email
         self.address = address
-
-    def toCSVList(self) -> list:
-        return [self.employee_id, self.name, self.location, self.phone, self.email, self.address]
-    
-    def toCommastring(self) -> str:
-        return f"{self.employee_id},{self.name},{self.location},{self.phone},{self.email},{self.address}\n"
+        self.opening_hours = opening_hours
 
