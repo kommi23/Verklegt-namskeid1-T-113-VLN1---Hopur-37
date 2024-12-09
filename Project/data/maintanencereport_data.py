24f7addce012cd3be795beab099ee4e95936e268
kommi23 <kormakurbjarna@gmail.com>
2024-12-09 17:35:34 +0000

    bætti við maintenece stuff

--- Project/data/maintanencereport_data.py
+++ Project/data/maintanencereport_data.py
@@ -9,7 +9,7 @@ class MaintenanceReportData:
                 csv_file.write(maintenancereport) 
                 return True
         except: raise        
-    def get_maintenanreport():
+    def get_maintenancereport():
         try:
             with open("maintenancereports.csv", "r", newline='', encoding='utf-8') as csv_file:
                 maintenancereports = []
@@ -18,3 +18,63 @@ class MaintenanceReportData:
                     maintenancereports.append(line)
             return maintenancereports
         except: raise 
+
+    def get_maintenancereports_by_property(property):
+        try:    
+                maintenencereports = []
+                with open("Project/data/csv_files/maintenancereports.csv", "r", newline='', encoding='utf-8') as csv_file:
+                    for line in csv_file:
+                        line = line.split(",")
+                        if property in line[-1]:
+                            maintenencereports.append(line)
+                    return maintenencereports
+            
+        except: raise  
+
+    def get_maintenancereports_by_date(date):
+        try:    
+                maintenencereports = []
+                with open("Project/data/csv_files/maintenancereports.csv", "r", newline='', encoding='utf-8') as csv_file:
+                    for line in csv_file:
+                        line = line.split(",")
+                        if date in line[1]:
+                            maintenencereports.append(line)
+                    return maintenencereports
+            
+        except: raise  
+
+    def get_maintenancereports_by_employee(employee_id):
+        try:    
+                maintenencereports = []
+                with open("Project/data/csv_files/maintenancereports.csv", "r", newline='', encoding='utf-8') as csv_file:
+                    for line in csv_file:
+                        line = line.split(",")
+                        if employee_id in line[2]:
+                            maintenencereports.append(line)
+                    return maintenencereports
+            
+        except: raise  
+
+    def get_maintenancereports_by_status(status):
+        try:    
+                maintenencereports = []
+                with open("Project/data/csv_files/maintenancereports.csv", "r", newline='', encoding='utf-8') as csv_file:
+                    for line in csv_file:
+                        line = line.split(",")
+                        if status in line[3]:
+                            maintenencereports.append(line)
+                    return maintenencereports
+            
+        except: raise  
+
+    def get_maintenancereports_by_priority(priority):
+        try:    
+                maintenencereports = []
+                with open("Project/data/csv_files/maintenancereports.csv", "r", newline='', encoding='utf-8') as csv_file:
+                    for line in csv_file:
+                        line = line.split(",")
+                        if priority in line[4]:
+                            maintenencereports.append(line)
+                    return maintenencereports
+            
+        except: raise  
--- Project/data/maintenancerequests_data.py
+++ Project/data/maintenancerequests_data.py
@@ -1,15 +1,17 @@
 import csv
 
-class MaintenanceData:
+class MaintenanceRequ:
     def __init__():
         pass
+
     def add_maintenance(maintenance):
         try:
             with open("maintenances.csv", "a", newline='', encoding='utf-8') as csv_file:
                 csv_file.write(maintenance)
                 return True 
         except: raise
-    def get_maintenences():
+
+    def get_maintenances():
         maintenances = []
         try:
             with open("maintenances.csv", "r", newline='', encoding='utf-8') as csv_file:
@@ -18,3 +20,41 @@ class MaintenanceData:
                     maintenances.append(line)
             return maintenances
         except: raise
+
+    def get_maintenancerequest_by_ID(ID):
+        try:    
+                maintenencerequests = []
+                with open("Project/data/csv_files/maintenancerequests.csv", "r", newline='', encoding='utf-8') as csv_file:
+                    for line in csv_file:
+                        line = line.split(",")
+                        if ID in line[0]:
+                            ID.append(line)
+                    return maintenencerequests
+            
+        except: raise  
+
+    def get_maintenancerequest_by_property_number(property_number):
+        try:    
+                maintenencerequests = []
+                with open("Project/data/csv_files/maintenancerequests.csv", "r", newline='', encoding='utf-8') as csv_file:
+                    for line in csv_file:
+                        line = line.split(",")
+                        if property_number in line[1]:
+                            maintenencerequests.append(line)
+                    return maintenencerequests
+            
+        except: raise  
+
+    def get_maintenancerequest_by_employee(employee_id):
+        try:    
+                maintenencerequests = []
+                with open("Project/data/csv_files/maintenancerequests.csv", "r", newline='', encoding='utf-8') as csv_file:
+                    for line in csv_file:
+                        line = line.split(",")
+                        if employee_id in line[0]:
+                            maintenencerequests.append(line)
+                    return maintenencerequests
+            
+        except: raise  
+
+    #Bæta við breyta verkbeiðni
