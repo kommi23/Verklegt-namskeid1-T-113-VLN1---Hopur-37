010c9c3165484ddeb4a38de7d204f0baba3adb10
kommi23 <kormakurbjarna@gmail.com>
2024-12-10 17:01:20 +0000

    bætti við location í data/models/csv

---
+++ Project/Models/Location.py
@@ -0,0 +1,15 @@
+class Location:
+    def __init__(self, location, country, airport, phone_number, opening_time, manager):
+        self.location = location
+        self.country = country
+        self.airport = airport
+        self.phone_number = phone_number
+        self.opening_time = opening_time
+        self.manager = manager
+
+    def turn_location_into_list(self):
+        return [self.location, self.country, self.airport, self.phone_number, self.opening_time, self.manager]
+    
+    def __str__(self) -> str:
+        return f"[{self.location}, {self.country}, {self.airport}, {self.phone_number},
+          {self.opening_time}, {self.manager},]"
